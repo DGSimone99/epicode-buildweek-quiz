b84450a6c7e674ee3d303f13be5eb19335da7e18
DGSimone99 <simone.digiorgio99@gmail.com>
2024-12-17 15:35:24 +0100

    Results

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,106 +13,108 @@ let corrette = 0
 let sbagliate = 0
 let totale = 0
 
+const questions = [
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What does CPU stand for?",
+    correct_answer: "Central Processing Unit",
+    incorrect_answers: [
+      "Central Process Unit",
+      "Computer Personal Unit",
+      "Central Processor Unit",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
+    correct_answer: "Final",
+    incorrect_answers: ["Static", "Private", "Public"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "The logo for Snapchat is a Bell.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question:
+      "Pointers were not used in the original C programming language; they were added later on in C++.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the most preferred image format used for logos in the Wikimedia database?",
+    correct_answer: ".svg",
+    incorrect_answers: [".png", ".jpeg", ".gif"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "In web design, what does CSS stand for?",
+    correct_answer: "Cascading Style Sheet",
+    incorrect_answers: [
+      "Counter Strike: Source",
+      "Corrective Style Sheet",
+      "Computer Style Sheet",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the code name for the mobile operating system Android 7.0?",
+    correct_answer: "Nougat",
+    incorrect_answers: [
+      "Ice Cream Sandwich",
+      "Jelly Bean",
+      "Marshmallow",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "On Twitter, what is the character limit for a Tweet?",
+    correct_answer: "140",
+    incorrect_answers: ["120", "160", "100"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "Linux was first created as an alternative to Windows XP.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "Which programming language shares its name with an island in Indonesia?",
+    correct_answer: "Java",
+    incorrect_answers: ["Python", "C", "Jakarta"],
+  },
+];
+
 if (document.location.pathname === "/quiz.html") {
-  const questions = [
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "What does CPU stand for?",
-      correct_answer: "Central Processing Unit",
-      incorrect_answers: [
-        "Central Process Unit",
-        "Computer Personal Unit",
-        "Central Processor Unit",
-      ],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question:
-        "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
-      correct_answer: "Final",
-      incorrect_answers: ["Static", "Private", "Public"],
-    },
-    {
-      category: "Science: Computers",
-      type: "boolean",
-      difficulty: "easy",
-      question: "The logo for Snapchat is a Bell.",
-      correct_answer: "False",
-      incorrect_answers: ["True"],
-    },
-    {
-      category: "Science: Computers",
-      type: "boolean",
-      difficulty: "easy",
-      question:
-        "Pointers were not used in the original C programming language; they were added later on in C++.",
-      correct_answer: "False",
-      incorrect_answers: ["True"],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question:
-        "What is the most preferred image format used for logos in the Wikimedia database?",
-      correct_answer: ".svg",
-      incorrect_answers: [".png", ".jpeg", ".gif"],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "In web design, what does CSS stand for?",
-      correct_answer: "Cascading Style Sheet",
-      incorrect_answers: [
-        "Counter Strike: Source",
-        "Corrective Style Sheet",
-        "Computer Style Sheet",
-      ],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question:
-        "What is the code name for the mobile operating system Android 7.0?",
-      correct_answer: "Nougat",
-      incorrect_answers: [
-        "Ice Cream Sandwich",
-        "Jelly Bean",
-        "Marshmallow",
-      ],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "On Twitter, what is the character limit for a Tweet?",
-      correct_answer: "140",
-      incorrect_answers: ["120", "160", "100"],
-    },
-    {
-      category: "Science: Computers",
-      type: "boolean",
-      difficulty: "easy",
-      question: "Linux was first created as an alternative to Windows XP.",
-      correct_answer: "False",
-      incorrect_answers: ["True"],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question:
-        "Which programming language shares its name with an island in Indonesia?",
-      correct_answer: "Java",
-      incorrect_answers: ["Python", "C", "Jakarta"],
-    },
-  ];
+
 
   const domanda = document.querySelector("#domanda")
   const risposte = document.querySelector(".risposte")
@@ -178,19 +180,55 @@ if (document.location.pathname === "/quiz.html") {
       }
       )
     }
-    
-  )
-  
+    )
   }
-
   caricaDomanda()
 };
 
 
 if (document.location.pathname === "/result.html") {
-  let result = document.querySelector("#corrette")
+  let corretteDiv = document.querySelector("#corrette")
+  let corrette = document.createElement("p")
   let corrette2 = localStorage.getItem("risposteCorrette")
-  result.innerText = corrette2
+  corretteDiv.appendChild(corrette)
+  corrette.innerText = (corrette2 / questions.length * 100) + "%"
+
+  let contatoreGiuste = document.createElement("p")
+  corretteDiv.appendChild(contatoreGiuste)
+  contatoreGiuste.innerText = corrette2 + "/" + questions.length + " questions"
+
+
+  let sbagliateDiv = document.querySelector("#sbagliate")
+  let sbagliate = document.createElement("p")
+  let sbagliate2 = localStorage.getItem("risposteSbagliate")
+  sbagliateDiv.appendChild(sbagliate)
+  sbagliate.innerText = (sbagliate2 / questions.length * 100) + "%"
+
+  let contatoreSbagliate = document.createElement("p")
+  sbagliateDiv.appendChild(contatoreSbagliate)
+  contatoreSbagliate.innerText = sbagliate2 + "/" + questions.length + " questions"
+
+
+
+  let risultato = document.querySelector("#risultato")
+
+  let superato = document.createElement("p")
+  let info = document.createElement("p")
+  superato.innerHTML = `Congratulations! <span class="superato">You passed the exam.</span>`
+  info.innerText = "We'll send you the certificate in few minutes. Check your email (including promotions / Spam folder)"
+
+  let nonSuperato = document.createElement("p")
+  let info2 = document.createElement("p")
+  nonSuperato.innerHTML = `Congratulations! <span class="superato">You didn't pass the exam.</span>`
+  info2.innerText = "We'll not send you the certificate in few minutes. Don't check your email (including promotions / Spam folder)"
+
+  if (contatoreGiuste.innerText >= "60%") {
+    risultato.appendChild(superato)
+    risultato.appendChild(info)
+  } else {
+    risultato.appendChild(nonSuperato)
+    risultato.appendChild(info2)
+  }
 }
 
 

40f10903bab2df406656fbe1166904f468357613
“Mark <mark.temblique@gmail.com>
2024-12-18 09:28:15 +0100

    js review unito al js generale

--- assets/script/script.js
+++ assets/script/script.js
@@ -261,3 +261,21 @@ if (document.location.pathname === "/result.html") {
     window.location.href = "review.html"
   })
 }
+
+//JS della pagina review
+
+const allStars = document.querySelectorAll(".star")
+
+ allStars.forEach((star, i) => {
+    star.onclick = function(){
+        let currentStarLevel = i + 1;
+
+        allStars.forEach ((star, x) =>{
+            if (currentStarLevel >= x+1) {
+                star.classList.add("starClicked");
+            } else {
+                star.classList.remove("starClicked");
+            }
+        })
+    }
+ })
